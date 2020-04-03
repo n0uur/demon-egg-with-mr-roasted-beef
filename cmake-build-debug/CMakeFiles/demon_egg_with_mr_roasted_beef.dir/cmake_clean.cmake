d3bb80807a2b30afc1589476e0e953211f0dc31a
n0uur <50010805+n0uur@users.noreply.github.com>
2020-04-03 17:53:11 +0700

    init project

---
+++ cmake-build-debug/CMakeFiles/demon_egg_with_mr_roasted_beef.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demon_egg_with_mr_roasted_beef.dir/main.c.obj"
+  "demon_egg_with_mr_roasted_beef.exe"
+  "demon_egg_with_mr_roasted_beef.exe.manifest"
+  "demon_egg_with_mr_roasted_beef.pdb"
+  "libdemon_egg_with_mr_roasted_beef.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demon_egg_with_mr_roasted_beef.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/demon_egg_with_mr_roasted_beef.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for demon_egg_with_mr_roasted_beef.
+# This may be replaced when dependencies are built.
