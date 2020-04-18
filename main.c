23819696555ea3570c1d9743ef1bb7244b9c86ed
n0uur <50010805+n0uur@users.noreply.github.com>
2020-04-18 20:35:08 +0700

    vs code settings

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - mouse painting");
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - mouse painting eiei");
 
     // Colours to choose from
     Color colors[MAX_COLORS_COUNT] = {
