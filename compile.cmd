f25c02285d494492719c6300bd667af02ce1c6b5
n0uur <50010805+n0uur@users.noreply.github.com>
2020-04-22 17:25:40 +0700

    updated compiler script for windows
    Include text.c to fixed Thai's vowels

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,4 @@
-gcc main.c game.c egg.c beef.c -I "raylib\src" -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows -o demonEgg
+@ECHO OFF
+gcc main.c game.c egg.c beef.c .\raylib\src\text.c -I "raylib\src" -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows -o demonEgg
+ECHO Finished!
 PAUSE
