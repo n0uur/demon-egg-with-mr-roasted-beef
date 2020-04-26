09179a26e35ee32973906b1a69f76c21454a0637
FuMiRiO <50404762+FuMiRiO@users.noreply.github.com>
2020-04-27 01:55:17 +0700

    hardcode -> loop

--- beef.c
+++ beef.c
@@ -7,71 +7,44 @@ void beefInit() {
     panImg = LoadImage("resources/roastedbeef/27_2.png");
     meatBowlImg = LoadImage("resources/roastedbeef/29.png");
     sauceBowlImg = LoadImage("resources/roastedbeef/32.png");
-    rawMeat1Img = LoadImage("resources/roastedbeef/raw_meat/62_r.png");
-    rawMeat2Img = LoadImage("resources/roastedbeef/raw_meat/68_r.png");
-    rawMeat3Img = LoadImage("resources/roastedbeef/raw_meat/74_r.png");
-    rawMeat4Img = LoadImage("resources/roastedbeef/raw_meat/80_r.png");
+    rawMeatImg[0] = LoadImage("resources/roastedbeef/raw_meat/62_r.png");
+    rawMeatImg[1] = LoadImage("resources/roastedbeef/raw_meat/68_r.png");
+    rawMeatImg[2] = LoadImage("resources/roastedbeef/raw_meat/74_r.png");
+    rawMeatImg[3] = LoadImage("resources/roastedbeef/raw_meat/80_r.png");
 
     //Resize for new screen size
     ImageResize(&tableImg, 1366, 720);
     ImageResize(&panImg, 1000, 400);
     ImageResize(&meatBowlImg, 500, 300);
     ImageResize(&sauceBowlImg, 400, 200);
-    ImageResize(&rawMeat1Img, 140, 88);
-    ImageResize(&rawMeat2Img, 140, 88);
-    ImageResize(&rawMeat3Img, 140, 88);
-    ImageResize(&rawMeat4Img, 140, 88);
+    for(int i = 0; i < 4; i++)
+        ImageResize(&rawMeatImg[i], 140, 88);
 
     //Load to Vram
     table = LoadTextureFromImage(tableImg);
     pan = LoadTextureFromImage(panImg);
     meatBowl = LoadTextureFromImage(meatBowlImg);
     sauceBowl = LoadTextureFromImage(sauceBowlImg);
-    rawMeat1 = LoadTextureFromImage(rawMeat1Img);
-    rawMeat2 = LoadTextureFromImage(rawMeat2Img);
-    rawMeat3 = LoadTextureFromImage(rawMeat3Img);
-    rawMeat4 = LoadTextureFromImage(rawMeat4Img);
-
+    for(int i = 0; i < 4; i++)
+        rawMeat[i] = LoadTextureFromImage(rawMeatImg[i]);
+    
     //Unload From RAM
     UnloadImage(tableImg);
     UnloadImage(panImg);
     UnloadImage(meatBowlImg);
     UnloadImage(sauceBowlImg);
-    UnloadImage(rawMeat1Img);
-    UnloadImage(rawMeat2Img);
-    UnloadImage(rawMeat3Img);
-    UnloadImage(rawMeat4Img);
+    for(int i = 0; i < 4; i++)
+        UnloadImage(rawMeatImg[i]);
 
     //Position of beef
-    beefArray[0].posX = SCREEN_WIDTH/2 - rawMeat1.width/2 - 400;
-    beefArray[0].posY = SCREEN_HEIGHT/2 - rawMeat1.height/2 + 240;
-    beefArray[1].posX = SCREEN_WIDTH/2 - rawMeat2.width/2 - 300;
-    beefArray[1].posY = SCREEN_HEIGHT/2 - rawMeat2.height/2 + 200;
-    beefArray[2].posX = SCREEN_WIDTH/2 - rawMeat3.width/2 - 400;
-    beefArray[2].posY = SCREEN_HEIGHT/2 - rawMeat3.height/2 + 180;
-    beefArray[3].posX = SCREEN_WIDTH/2 - rawMeat4.width/2 - 500;
-    beefArray[3].posY = SCREEN_HEIGHT/2 - rawMeat4.height/2 + 180;
-
-    rectanglePosX[0] = beefArray[0].posX + 18;
-    rectanglePosX[1] = beefArray[1].posX + 25;
-    rectanglePosX[2] = beefArray[2].posX + 15;
-    rectanglePosX[3] = beefArray[3].posX + 25;
-
-    rectanglePosY[0] = beefArray[0].posY;
-    rectanglePosY[1] = beefArray[1].posY;
-    rectanglePosY[2] = beefArray[2].posY - 4;
-    rectanglePosY[3] = beefArray[3].posY;
-
-    rectangleWidth[0] = rawMeat1.width - 23;
-    rectangleWidth[1] = rawMeat2.width - 26;
-    rectangleWidth[2] = rawMeat1.width - 23;
-    rectangleWidth[3] = rawMeat1.width - 35;
-
-    rectangleHeight[0] = rawMeat1.height - 10;
-    rectangleHeight[1] = rawMeat2.height - 5;
-    rectangleHeight[2] = rawMeat1.height - 10;
-    rectangleHeight[3] = rawMeat1.height;
-
+    for(int i = 0; i < 4; i++){
+        beefArray[i].posX = SCREEN_WIDTH/2 - rawMeatImg[i].width/2 + calcBeefArrayPosX[i];
+        beefArray[i].posY = SCREEN_HEIGHT/2 - rawMeatImg[i].height/2 + calcBeefArrayPosY[i];
+        rectanglePosX[i] = beefArray[i].posX + calcRectanglePosX[i];
+        rectanglePosY[i] = beefArray[i].posY + calcRectanglePosY[i];
+        rectangleHeight[i] = rawMeat[i].height + calcRectangleHeight[i];
+        rectangleWidth[i] = rawMeat[i].width + calcRectangleWidth[i];
+    }
 }
 
 void beefMain() {
@@ -86,17 +59,10 @@ void beefMain() {
         DrawTexture(meatBowl, SCREEN_WIDTH/2 - meatBowl.width/2 - 400, SCREEN_HEIGHT/2 - meatBowl.height/2 + 240, WHITE);
         DrawTexture(sauceBowl, SCREEN_WIDTH/2 - sauceBowl.width/2 + 400, SCREEN_HEIGHT/2 - sauceBowl.height/2 + 280, WHITE);
         
-        DrawTexture(rawMeat1, beefArray[0].posX, beefArray[0].posY, WHITE);
-        DrawRectangleLines(rectanglePosX[0], rectanglePosY[0], rectangleWidth[0], rectangleHeight[0], BLANK);
-        
-        DrawTexture(rawMeat2, beefArray[1].posX, beefArray[1].posY, WHITE);
-        DrawRectangleLines(rectanglePosX[1], rectanglePosY[1], rectangleWidth[1], rectangleHeight[1], BLANK);
-
-        DrawTexture(rawMeat3, beefArray[2].posX, beefArray[2].posY, WHITE);
-        DrawRectangleLines(rectanglePosX[2], rectanglePosY[2], rectangleWidth[2], rectangleHeight[2], BLANK);
-
-        DrawTexture(rawMeat4, beefArray[3].posX, beefArray[3].posY, WHITE);
-        DrawRectangleLines(rectanglePosX[3], rectanglePosY[3], rectangleWidth[3], rectangleHeight[3], WHITE);
+        for(int i = 0; i < 4; i++){
+            DrawTexture(rawMeat[i], beefArray[i].posX, beefArray[i].posY, WHITE);
+            DrawRectangleLines(rectanglePosX[i], rectanglePosY[i], rectangleWidth[i], rectangleHeight[i], WHITE);
+        }
 
     EndDrawing();
 }
--- beef.h
+++ beef.h
@@ -23,10 +23,7 @@ Image tableImg;
 Image panImg;
 Image meatBowlImg;
 Image sauceBowlImg;
-Image rawMeat1Img;
-Image rawMeat2Img;
-Image rawMeat3Img;
-Image rawMeat4Img;
+Image rawMeatImg[4];
 
 // -----------------------
 
@@ -34,10 +31,7 @@ Texture2D table;
 Texture2D pan;
 Texture2D meatBowl;
 Texture2D sauceBowl;
-Texture2D rawMeat1;
-Texture2D rawMeat2;
-Texture2D rawMeat3;
-Texture2D rawMeat4;
+Texture2D rawMeat[4];
 
 // -----------------------
 
@@ -48,10 +42,17 @@ struct beef {
     enum STATE CURRENT_MEAT_STATE;
 } beefArray[MEAT_COUNT];
 
-int rectanglePosX[10];
-int rectanglePosY[10];
-int rectangleHeight[10];
-int rectangleWidth[10];
+int rectanglePosX[MEAT_COUNT];
+int rectanglePosY[MEAT_COUNT];
+int rectangleHeight[MEAT_COUNT];
+int rectangleWidth[MEAT_COUNT];
+
+int calcBeefArrayPosX[MEAT_COUNT] = {-400, -300, -400, -500};
+int calcBeefArrayPosY[MEAT_COUNT] = {240, 200, 180, 180};
+int calcRectanglePosX[MEAT_COUNT] = {18, 25, 15, 25};
+int calcRectanglePosY[MEAT_COUNT] = {0, 0, -4, 0};
+int calcRectangleHeight[MEAT_COUNT] = {-10, -5, -10, 0};
+int calcRectangleWidth[MEAT_COUNT] = {-23, -26, -23, -35};
 
 //-------------------------
 
