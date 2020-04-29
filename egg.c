5f702cec005faadeb0a42794f85b6a3dcc8f4a33
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2020-04-29 18:23:27 +0700

    egg now can jump

--- egg.c
+++ egg.c
@@ -1,7 +1,10 @@
 #include "egg.h"
+#include <math.h>
 
 void eggInit()
 {
+    //----------------------------
+
     // Loaded in CPU memory
     eggImg = LoadImage("resources/dozenegg/images/bigger_egg.png");
     basketImg = LoadImage("resources/dozenegg/images/egg.png");
@@ -28,36 +31,80 @@ void eggInit()
     UnloadImage(goalImg);
     UnloadImage(eggInBasket);
     UnloadImage(auraImg);
+
+    //----------------------------
+
+    int posX = 1366 / 2;
+    int posY = 768 / 2;
+
+    CURRENT_EGG_STATE = EGG_WAIT;
+
+    floorPositionY = 720;
+    levelHeight = 400;
+    jumpHeight = 60;
+    baseLevelY = 730;
+
+    gravity = 15;
+    velocityY = 0;
+
+    //----------------------------
+
+    for(int i = 0; i < 150; i++) {
+
+    }
 }
 
 void eggMain()
 {
+    //----------------------------
+    //-- ไข่
+    //----------------------------
 
-    if (IsKeyDown(KEY_LEFT))
-        posX -= 5;
-    else if (IsKeyDown(KEY_RIGHT))
-        posX += 5;
-    int currentPositionY = posY;
-    if (GetTime() - lastPressedSpace < 1)
-    {
-        posY -= 100.0 * 0.0166;
+    if(CURRENT_EGG_STATE == EGG_WAIT) {
+        // GRAVITY
+        posY = baseLevelY;
+
+        // Check for new state logic
+        if (IsKeyDown(KEY_UP)) {
+            CURRENT_EGG_STATE = EGG_JUMP;
+            velocityY = 0;
+        }
     }
-    if (GetTime() - lastPressedSpace > 1 && GetTime() - lastPressedSpace < 2)
-    {
-        posY += 100.0 * 0.0166;
+    else if(CURRENT_EGG_STATE == EGG_JUMP) {
+        // GRAVITY
+        velocityY -= gravity * GetFrameTime();
+        posY += velocityY * abs(posY - jumpHeight) * 0.05;
+
+        // Check for new state logic
+        if(posY <= jumpHeight) {
+            CURRENT_EGG_STATE = EGG_FALL;
+            velocityY = 0;
+        }
     }
-    if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_SPACE))
-    {
-        if (GetTime() - lastPressedSpace >= 2)
-        {
-            lastPressedSpace = GetTime();
+    else if(CURRENT_EGG_STATE == EGG_FALL) {
+        // GRAVITY
+        velocityY += gravity * GetFrameTime();
+        posY += velocityY;
+
+        // Check for new state logic
+        if(posY >= baseLevelY) {
+            CURRENT_EGG_STATE = EGG_WAIT;
+            posY = baseLevelY;
         }
     }
 
+    //----------------------------
+    //-- ด่าน
+    //----------------------------
+
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
-    DrawTexture(eggTexture, posX, posY, WHITE);
+        ClearBackground(RAYWHITE);
+        DrawTexture(eggTexture, posX, posY, WHITE);
 
     EndDrawing();
+}
+
+void resetGravity() {
+    gravity = 15;
 }
--- egg.h
+++ egg.h
@@ -4,7 +4,13 @@ int posX = 1366 / 2;
 int posY = 768 / 2;
 double lastPressedSpace = 0;
 
-//Image
+// -----------------------
+
+int eggPositionX;
+int eggPositionY;
+
+// -----------------------
+
 Image eggImg;
 Image basketImg;
 Image backgroundImg;
@@ -12,10 +18,51 @@ Image goalImg;
 Image eggInBasket;
 Image auraImg;
 
-//Texture2d
+// -----------------------
+
 Texture2D eggTexture;
 Texture2D basketTexture;
 Texture2D backgroundTexture;
 Texture2D goalTexture;
 Texture2D eggInBasketTexture;
 Texture2D auraTexture;
+
+// -----------------------
+
+enum GAME_PLAY_STATE {
+    EGG_PLAYING,
+    EGG_GAME_OVER
+};
+
+enum EGG_PLAY_STATE {
+    EGG_WAIT,
+    EGG_JUMP,
+    EGG_FALL,
+    EGG_NEXT_LEVEL_TRANSITION,
+    EGG_FAIL_TRANSITION
+};
+
+enum LEVEL_MOVEMENT_TYPE {
+    MOVE_STATIC,
+    MOVE_LEFT_RIGHT
+};
+
+enum EGG_PLAY_STATE CURRENT_EGG_STATE;
+// -----------------------
+
+int floorPositionY;
+int levelHeight;
+int jumpHeight;
+int baseLevelY;
+
+float gravity;
+float velocityY;
+
+// -----------------------
+
+struct Level {
+    Vector2 position;
+    enum LEVEL_MOVEMENT_TYPE movementType;
+};
+
+struct Level gameLevels[150];
