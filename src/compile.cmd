7d7c3a997a7b0a4ae11d6b363e8c0a273dc827da
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2020-05-01 18:57:09 +0700

    add file's info and resource

--- src/compile.cmd
+++ src/compile.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Compiling...
-gcc main.c game.c egg.c beef.c .\external\raylib\src\text.c -I "external\raylib\src" -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -mwindows -o ..\bin\demonEgg
+gcc main.c game.c egg.c beef.c .\external\raylib\src\text.c -I "external\raylib\src" -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -mwindows -o ..\bin\demonEgg resource\resouce.res
 ECHO Finished!
 PAUSE
